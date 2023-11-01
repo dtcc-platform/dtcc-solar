adc3d60bdc9e49ea37df4103fe913119832264eb
Jens Olsson <jens.olsson@chalmersindustriteknik.se>
2023-11-01 11:30:26 +0100

    Fix in init-file to remove CI error

--- src/dtcc_solar/__init__.py
+++ src/dtcc_solar/__init__.py
@@ -3,7 +3,7 @@ from .sunpath import Sunpath
 from .sundome import SunDome
 from .skydome import SkyDome
 
-from .viewer import Viewer as SolarViewer
+# from .viewer import Viewer as SolarViewer
 from .utils import Sun, SunQuad, SolarParameters, DataSource
 
 
@@ -18,5 +18,5 @@ __all__ = [
     "SunQuad",
     "SolarParameters",
     "DataSource",
-    "SolarViewer",
+    # "SolarViewer",
 ]
